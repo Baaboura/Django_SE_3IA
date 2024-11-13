906be44c5e1bb6bb1d5648307d3ec3e358c70cf9
IdrissBenmoussa <131977412+IdrissBenmoussa@users.noreply.github.com>
2024-11-13 09:20:54 +0100

    added needed stuff

--- Produit/urls.py
+++ Produit/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('add-to-commande/', views.click_button_add_to_commande, name='click_button_add_to_commande'),
 path('delete_commande/<int:id>/', views.delete_commande, name='delete_commande'),
     path('edit_commande/<int:commande_id>/', views.edit_commande, name='edit_commande'),
+    path('update-quantity/<int:commande_id>/', views.update_quantity, name='update_quantity'),
+
 ]
